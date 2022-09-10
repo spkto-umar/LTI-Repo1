b2115304aa4077cc57717a7e7842bb99d642d1b8
spkto-umar <spk2umar01@gmail.com>
2022-09-10 17:30:46 +0530

    adding demo file

---
+++ Maven_Employee/src/main/java/com/lti/Maven_Employee/Demo2.java
@@ -0,0 +1,10 @@
+package com.lti.Maven_Employee;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("just adding after commit1");
+	}
+
+}
